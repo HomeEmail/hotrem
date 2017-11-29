7150501ad23f6d8943fd7d35f41e41436def93e2
HomeEmail <591694604@qq.com>
2017-11-29 18:46:47 +0800

    add readme

--- README.md
+++ README.md
@@ -17,3 +17,26 @@ yarn add hotrem
 
 或者手动复制hotrem.js到你项目里引用
 
+
+用法:
+```
+//引入js文件后 
+hotrem.init({
+    //这里设置初始化参数,或者在html页面里得meta里设置
+    designWidth:640,
+    maxWidth:640,
+    rootFontSize:40
+});
+//执行下初始化,再执行
+hotrem.listener();
+//即可
+
+```
+
+初始化参数,或者在html页面里得meta里设置
+
+```
+<meta name="hotrem" content="design-width=640px,max-width=640px,initial-dpr=2,root-fontsize=40" />
+
+```
+
--- hotrem.js
+++ hotrem.js
@@ -2,22 +2,22 @@
 
     'use strict';
 
-    //注意：这个js默认使用的是基准屏幕宽度为320px,font-size为20px的标准下进行适配其他屏幕尺寸的
+    //注意：这个js默认使用的是基准屏幕宽度为640px,font-size为40px的标准下进行适配其他屏幕尺寸的
 
     //用法:引入js文件后 hotrem.init()执行下初始化,再执行hotrem.listener(); 即可 
 
     //给hotrem开辟个命名空间，别问我为什么，我要给你准备你会用到的方法，免得用到的时候还要自己写。
     var hotrem = {};
 
-    hotrem.init=function() {
+    hotrem.init=function(op) {
         //根据devicePixelRatio自定计算scale
         //可以有效解决移动端1px这个世纪难题。
         var viewportEl = document.querySelector('meta[name="viewport"]'),
             hotremEl = document.querySelector('meta[name="hotrem"]'),
             dpr = window.devicePixelRatio || 1,
-            maxWidth = 540,
-            designWidth = 640,
-            rootFontSize = 40
+            maxWidth = op.maxWidth||540,
+            designWidth = op.designWidth||640,
+            rootFontSize = op.rootFontSize||40
         ;
 
         dpr = dpr >= 3 ? 3 : ( dpr >=2 ? 2 : 1 );
@@ -38,10 +38,14 @@
                 if (designWidthMatch) {
                     designWidth = parseFloat(designWidthMatch[1]);
                 }
+                var rootFontSizeMatch = hotremCon.match(/root\-fontsize=([\d\.]+)/);
+                if (rootFontSizeMatch) {
+                    rootFontSize = parseFloat(rootFontSizeMatch[1]);
+                }
             }
         }
 
-        //<meta name="hotrem" content="design-width=640px,max-width=640px,initial-dpr=2" />
+        //<meta name="hotrem" content="design-width=640px,max-width=640px,initial-dpr=2,root-fontsize=40" />
 
         document.documentElement.setAttribute('data-dpr', dpr);
         hotrem.dpr = dpr;
@@ -54,8 +58,10 @@
         }
         hotrem.designWidth = designWidth; // 保证px2rem 和 rem2px 不传第二个参数时, 获取hotrem.designWidth是undefined导致的NaN
 
+        hotrem.rootFontSize = rootFontSize;
+
         var scale = 1 / dpr,
-            content = 'width=device-width, initial-scale=' + scale + ', minimum-scale=' + scale + ', maximum-scale=' + scale + ', user-scalable=no';
+            content = 'width=device-width, initial-scale=' + scale + ', minimum-scale=' + scale + ', maximum-scale=' + scale + ', user-scalable=no,minimal-ui';
 
         if (viewportEl) {
             viewportEl.setAttribute('content', content);
@@ -69,43 +75,41 @@
     };
 
     hotrem.px2rem = function( px , designWidth ){
-        //预判你将会在JS中用到尺寸，特提供一个方法助你在JS中将px转为rem。就是这么贴心。
+        //在JS中将px转为rem
         if( !designWidth ){
-            //如果你在JS中大量用到此方法，建议直接定义 hotrem.designWidth 来定义设计图尺寸;
-            //否则可以在第二个参数告诉我你的设计图是多大。
-            designWidth = parseInt(hotrem.designWidth , 10);
+
+            designWidth = parseInt(this.designWidth , 10);
         }
 
-        return (parseInt(px,10)/20)*(320/designWidth);
+        return (parseInt(px,10)/this.rootFontSize)*(this.designWidth/designWidth);
     }
 
     hotrem.rem2px = function( rem , designWidth ){
-        //新增一个rem2px的方法。用法和px2rem一致。
+
         if( !designWidth ){
-            designWidth = parseInt(hotrem.designWidth , 10);
+            designWidth = parseInt(this.designWidth , 10);
         }
-        //rem可能为小数，这里不再做处理了
-        return rem*20*designWidth/320;
+        return rem*this.rootFontSize*designWidth/this.designWidth;
     }
 
     hotrem.mresize = function(){
         //对，这个就是核心方法了，给HTML设置font-size。
         var innerWidth = document.documentElement.getBoundingClientRect().width || window.innerWidth;
 
-        if( hotrem.maxWidth && (innerWidth/hotrem.dpr > hotrem.maxWidth) ){
-            innerWidth = hotrem.maxWidth*hotrem.dpr;
+        if( this.maxWidth && (innerWidth/this.dpr > this.maxWidth) ){
+            innerWidth = this.maxWidth*this.dpr;
         }
 
         if( !innerWidth ){ return false;}
 
-        document.documentElement.style.fontSize = ( (innerWidth/320)*20 ) + 'px';
+        document.documentElement.style.fontSize = ( (innerWidth/this.designWidth)*this.rootFontSize ) + 'px';
 
-        hotrem.callback && hotrem.callback();
+        this.callback && this.callback();
 
     };
 
     hotrem.listener=function(){
-        hotrem.mresize(); 
+        this.mresize();
         //直接调用一次
 
         //var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
@@ -128,7 +132,6 @@
 
     
 
-    //window.hotrem = hotrem; 
     //命名空间暴露给你，控制权交给你，想怎么调怎么调。
     if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
         define(function() {
